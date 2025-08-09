b5cb5e694e9da5c683d62f7b484e4e82e98e946e
sanjograiofficial <sanjograiofficial@gmail.com>
2025-08-09 20:45:59 +0545

    Fix: fixed some frontend-backend connection issues

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,18 @@ const { chats } = require("./data/data");
 const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
     res.send("API is running");
 });
-
-
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
+});
+app.get("/api/chat/:id", (req, res) => {
+    const singleChat = chats.find((c) => c._id == req.params.id);
+    res.send(singleChat);
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log("Server started on port ", port);
